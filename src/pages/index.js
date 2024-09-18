980d657bfaac9d73a4bd103290018c919213959c
Deilons <apolo4748@email.com>
2024-09-17 19:32:52 -0500

    Feat: route to Terms and conditions page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import TourSelection from './TourSelection';
 import DateReserve from './DateReserve';
 import AboutUs from './AboutUs';
 import FrequentQuestion from './FrequentQuestions';
+import TermsAndConditions from './TermsAndConditions';
 
 export {
   Home,
@@ -22,6 +23,7 @@ export {
   TourSelection,
   DateReserve,
   AboutUs,
-  FrequentQuestion
+  FrequentQuestion,
+  TermsAndConditions
   // Reservations,
 };
